a71c5127968984416b5218b38eb2ea5dd01d0d5f
Ashish Sahu <aashish.sahu2.6@gmail.com>
2025-12-19 13:07:18 +0530

    nav last item

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import CartButton from "@/components/common/CartButton";
 import SearchBar from "@/components/common/SearchBar";
 import UserProfile from "@/components/common/UserProfile";
@@ -6,6 +8,8 @@ import AnnouncementBar from "@/components/common/AnnouncementBar";
 import Link from "next/link";
 
 const Header = () => {
+  const [search, setSearch] = useState("");
+
   return (
     <div className="w-full border-b bg-background bg-gradient-to-b from-emerald-50 via-cyan-50 to-amber-50  sticky top-0 z-50">
       <AnnouncementBar />
@@ -23,11 +27,11 @@ const Header = () => {
             </Link>
           </div>
           <div className="flex-1 hidden md:block max-w-xl">
-            <SearchBar />
+           <SearchBar onSearch={setSearch} />
           </div>
           <div className="flex items-center gap-4">
             <CartButton />
-            
+
             <UserProfile />
           </div>
         </div>
